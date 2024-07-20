c4a5a973433c1b64d5615edd47c2357aed3485cd
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-07-20 11:16:22 +0900

    init: tokenizer

---
+++ src/main.rs
@@ -0,0 +1,240 @@
+fn main() {
+    let src = std::fs::read_to_string("./sample_bloom_extract.csh").expect("Failed to load source");
+    let mut tokenizer = Tokenizer {
+        source: &src,
+        line: 0,
+        col: 0,
+        current_line_indent: 0,
+    };
+    // populate line indent for first line
+    tokenizer.populate_line_indent();
+    while let Some(t) = tokenizer.next_token().unwrap() {
+        println!("tok: {t:?}");
+    }
+}
+
+#[derive(Debug)]
+pub enum TokenizerErrorKind {
+    IncompleteHexLiteral,
+}
+
+#[derive(Debug)]
+pub struct TokenizerError {
+    pub kind: TokenizerErrorKind,
+    pub line: usize,
+    pub col: usize,
+}
+
+struct Tokenizer<'s> {
+    pub source: &'s str,
+    pub line: usize,
+    pub col: usize,
+    pub current_line_indent: usize,
+}
+impl<'s> Tokenizer<'s> {
+    pub fn populate_line_indent(&mut self) {
+        let (line_indent_chars, line_indent_bytes) = self
+            .source
+            .chars()
+            .take_while(|&c| c.is_whitespace() && c != '\n')
+            .fold((0, 0), |(a, b), c| (a + 1, b + c.len_utf8()));
+        self.current_line_indent = line_indent_chars;
+        self.col = line_indent_chars;
+        self.source = &self.source[line_indent_bytes..];
+    }
+
+    pub fn next_token(&mut self) -> Result<Option<Token<'s>>, TokenizerError> {
+        let (head_space_chars, head_space_bytes) = self
+            .source
+            .chars()
+            .take_while(|&c| c.is_whitespace() && c != '\n')
+            .fold((0, 0), |(a, b), c| (a + 1, b + c.len_utf8()));
+        self.col += head_space_chars;
+        self.source = &self.source[head_space_bytes..];
+
+        while self.source.starts_with('\n') {
+            let lf_count = self
+                .source
+                .chars()
+                .take_while(|&c| c == '\n' || c == '\r')
+                .fold(0, |a, c| a + (if c == '\n' { 1 } else { 0 }));
+            self.line += lf_count;
+            self.source = self.source.trim_start_matches(|c| c == '\n' || c == '\r');
+            self.populate_line_indent();
+        }
+
+        if self.source.is_empty() {
+            return Ok(None);
+        }
+
+        if self.source.starts_with("->") {
+            let tk = Token {
+                slice: &self.source[..2],
+                kind: TokenKind::ArrowToRight,
+                line: self.line,
+                col: self.col,
+                line_indent: self.current_line_indent,
+            };
+            self.source = &self.source[2..];
+            self.col += 2;
+            return Ok(Some(tk));
+        }
+
+        if self.source.starts_with("==")
+            || self.source.starts_with("!=")
+            || self.source.starts_with("<=")
+            || self.source.starts_with(">=")
+            || self.source.starts_with("&&")
+            || self.source.starts_with("||")
+        {
+            let tk = Token {
+                slice: &self.source[..2],
+                kind: TokenKind::Op,
+                line: self.line,
+                col: self.col,
+                line_indent: self.current_line_indent,
+            };
+            self.source = &self.source[2..];
+            self.col += 2;
+            return Ok(Some(tk));
+        }
+
+        let single_tok = match self.source.as_bytes()[0] {
+            b'[' => Some(TokenKind::OpenBracket),
+            b']' => Some(TokenKind::CloseBracket),
+            b'(' => Some(TokenKind::OpenParenthese),
+            b')' => Some(TokenKind::CloseParenthese),
+            b',' => Some(TokenKind::Comma),
+            b':' => Some(TokenKind::Colon),
+            b'=' => Some(TokenKind::Eq),
+            b'+' | b'-' | b'*' | b'/' | b'%' | b'&' | b'|' | b'^' | b'~' | b'!' | b'<' | b'>'
+            | b'.' => Some(TokenKind::Op),
+            _ => None,
+        };
+        if let Some(k) = single_tok {
+            let tk = Token {
+                slice: &self.source[..1],
+                kind: k,
+                line: self.line,
+                col: self.col,
+                line_indent: self.current_line_indent,
+            };
+            self.source = &self.source[1..];
+            self.col += 1;
+            return Ok(Some(tk));
+        }
+
+        if self.source.starts_with("0x") || self.source.starts_with("0X") {
+            // hexlit
+            let hexpart_count = self.source[2..]
+                .chars()
+                .take_while(|c| {
+                    ('0'..='9').contains(c) || ('a'..='f').contains(c) || ('A'..='F').contains(c)
+                })
+                .count();
+            if hexpart_count == 0 {
+                return Err(TokenizerError {
+                    kind: TokenizerErrorKind::IncompleteHexLiteral,
+                    line: self.line,
+                    col: self.col,
+                });
+            }
+
+            let tk = Token {
+                slice: &self.source[..2 + hexpart_count],
+                kind: TokenKind::Number,
+                line: self.line,
+                col: self.col,
+                line_indent: self.current_line_indent,
+            };
+            self.source = &self.source[2 + hexpart_count..];
+            self.col += 2 + hexpart_count;
+            return Ok(Some(tk));
+        }
+
+        if self.source.starts_with(|c: char| ('0'..='9').contains(&c)) {
+            let ipart_count = self
+                .source
+                .chars()
+                .take_while(|c| ('0'..='9').contains(c))
+                .count();
+            let extended_count = if self.source[ipart_count..].starts_with('.') {
+                1 + self.source[ipart_count + 1..]
+                    .chars()
+                    .take_while(|c| ('0'..='9').contains(c))
+                    .count()
+            } else {
+                0
+            };
+
+            let has_float_suffix =
+                self.source[ipart_count + extended_count..].starts_with(['f', 'F']);
+            let total_length = ipart_count + extended_count + if has_float_suffix { 1 } else { 0 };
+
+            let tk = Token {
+                slice: &self.source[..total_length],
+                kind: if has_float_suffix {
+                    TokenKind::Float
+                } else {
+                    TokenKind::Number
+                },
+                line: self.line,
+                col: self.col,
+                line_indent: self.current_line_indent,
+            };
+            self.source = &self.source[total_length..];
+            self.col += total_length;
+            return Ok(Some(tk));
+        }
+
+        let (ident_char_count, ident_byte_count) = self
+            .source
+            .chars()
+            .take_while(|&c| !"=!\"#$%&'()[]_?><.,;:@=~-^|\\ \t\r\n".contains(c))
+            .fold((0, 0), |(a, b), c| (a + 1, b + c.len_utf8()));
+        assert!(
+            ident_byte_count > 0,
+            "empty identifier token(src: {}...)",
+            &self.source[..8]
+        );
+        let tk = Token {
+            slice: &self.source[..ident_byte_count],
+            kind: match &self.source[..ident_byte_count] {
+                "struct" | "with" | "if" | "else" | "then" | "do" | "let" => TokenKind::Keyword,
+                _ => TokenKind::Identifier,
+            },
+            line: self.line,
+            col: self.col,
+            line_indent: self.current_line_indent,
+        };
+        self.source = &self.source[ident_byte_count..];
+        self.col += ident_char_count;
+        Ok(Some(tk))
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum TokenKind {
+    Identifier,
+    Keyword,
+    Op,
+    Float,
+    Number,
+    OpenBracket,
+    CloseBracket,
+    OpenParenthese,
+    CloseParenthese,
+    Comma,
+    Colon,
+    ArrowToRight,
+    Eq,
+}
+
+#[derive(Debug)]
+struct Token<'s> {
+    pub slice: &'s str,
+    pub kind: TokenKind,
+    pub line: usize,
+    pub col: usize,
+    pub line_indent: usize,
+}
